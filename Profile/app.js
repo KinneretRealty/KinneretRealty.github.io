855925f099e65fd354e2b8df6c43b772b80f2a5b
Ikey Benzaken <ikey.benz@gmail.com>
2018-01-31 22:51:40 -0500

    Added a step toggle button and step two content
    In step two you can pick your area
    There’s now a button to toggle between steps
    Java Script edits the progress bar, and the title of the next step
    button, as well as changing which step is visible

--- Profile/app.js
+++ Profile/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-
+    $('#NextButton').click(toggleStep)
 })
+
+var currentStep = 0;
+function toggleStep() {
+    if (currentStep < 4) {currentStep += 1}
+    else {currentStep = 0}
+    const steps = ['One', 'Two','Three','Four','Five'];
+    for (i in steps) {
+        document.getElementById(`${steps[i]}`).style.display = 'none';
+    }
+    document.getElementById(`${steps[currentStep]}`).style.display = 'block';
+    document.getElementById('NextButton').innerHTML = `${currentStep + 1}/5>`;
+    document.getElementById('ProgressColor').style.width = `calc(${(currentStep + 1)*100}% / 5)`;
+
+}
